2befed5c57ec1ec8c9d614c6ff4ee65fba375c8d
shmm0320 <shmm0320@gmail.com>
2025-05-07 17:56:24 +0900

    feat : PWA 기능 추가

--- FE/beecareful/vite.config.ts
+++ FE/beecareful/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: '비케어풀 - 꿀벌 통합 관리 시스템',
+        short_name: '비케어풀',
+        theme_color: '#F7D143',
+        icons: [
+          {
+            src: '/beecareful-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/beecareful-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
