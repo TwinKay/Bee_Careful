fc241b09beb1ad098936af09c84e4063a1a4394c
dkandjsl <dkandjsl@gmail.com>
2025-05-08 22:23:11 +0900

    feat: 검사 상세 페이지 생성

---
+++ FE/beecareful/src/components/diagnosis/DiagnosisList.tsx
@@ -0,0 +1,9 @@
+const DiagnosisList = () => {
+  return (
+    <div>
+      <h1>Diagnosis List</h1>
+      {/* Add your diagnosis list content here */}
+    </div>
+  );
+};
+export default DiagnosisList;
