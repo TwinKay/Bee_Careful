548c74476ba09a1b89ec76f914eb95ecfe9f09fc
dkandjsl <dkandjsl@gmail.com>
2025-05-08 23:13:52 +0900

    feat: 사진 추가, 삭제 기능

--- FE/beecareful/src/layouts/MainLayout.tsx
+++ FE/beecareful/src/layouts/MainLayout.tsx
@@ -1,18 +1,12 @@
 import { Outlet, useNavigate } from 'react-router-dom';
-import { RiArrowLeftSLine } from '@remixicon/react';
 import RemixIcon from '@/components/common/RemixIcon';
 
 const MainLayout = (icon?: string) => {
   const route = useNavigate();
   return (
     <div className="flex h-screen flex-col bg-gray-50">
-      <header className="flex flex-row items-center justify-between border-b border-gray-300 bg-white p-4">
-        <RiArrowLeftSLine
-          size={28}
-          onClick={() => {
-            route(-1);
-          }}
-        />
+      <header className="flex h-16 flex-row items-center justify-between border-b border-gray-300 bg-white p-4">
+        <RemixIcon name={'ri-arrow-left-s-line'} onClick={() => route(-1)} />
         {icon && <RemixIcon name={icon} />}
       </header>
       <Outlet />
