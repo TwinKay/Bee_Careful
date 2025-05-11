a1424df2f816ac4149c56a96d341294872d366fa
dkandjsl <dkandjsl@gmail.com>
2025-05-11 19:17:05 +0900

    feat: 진단 내역 목록

--- FE/beecareful/src/components/diagnosis/DiagnosisList.tsx
+++ FE/beecareful/src/components/diagnosis/DiagnosisList.tsx
@@ -1,8 +1,22 @@
-const DiagnosisList = () => {
+import DiagnosisListItem from './DiagnosisListItem';
+
+export type DiagnosisListPropsType = {
+  data: {
+    id: number;
+    date: string;
+    '응애(진드기)': number;
+    부저병: number;
+    석고병: number;
+    날개바이러스: number;
+  }[];
+};
+const DiagnosisList: React.FC<DiagnosisListPropsType> = ({ data }) => {
+  data.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
   return (
-    <div>
-      <h1>Diagnosis List</h1>
-      {/* Add your diagnosis list content here */}
+    <div className="flex w-full flex-col gap-8">
+      {data.map((diagnosis) => (
+        <DiagnosisListItem key={diagnosis.id} diagnosis={diagnosis} />
+      ))}
     </div>
   );
 };
--- FE/beecareful/src/components/diagnosis/DiagnosisListItem.tsx
+++ FE/beecareful/src/components/diagnosis/DiagnosisListItem.tsx
@@ -1,9 +1,40 @@
-const DiagnosisListItem = () => {
+import { Link } from 'react-router-dom';
+import Button from '../common/Button';
+import Card from '../common/Card';
+import { ROUTES } from '@/config/routes';
+
+export type DiagnosisListItemPropsType = {
+  diagnosis: {
+    id: number;
+    date: string;
+    '응애(진드기)': number;
+    부저병: number;
+    석고병: number;
+    날개바이러스: number;
+  };
+};
+const DiagnosisListItem: React.FC<DiagnosisListItemPropsType> = ({
+  diagnosis: { id, date, '응애(진드기)': mite, 부저병: buzz, 석고병: gypsum, 날개바이러스: virus },
+}) => {
+  const localeDateString = new Date(date).toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+  });
   return (
-    <div>
-      <h1>Diagnosis ListItem</h1>
-      {/* Add your diagnosis listItem content here */}
-    </div>
+    <Link to={ROUTES.DIAGNOSIS_DETAIL}>
+      <Card className="flex-row items-center justify-between bg-gray-100 p-4 py-4">
+        <div className="flex w-full justify-between">
+          <div className="flex flex-col items-start gap-1">
+            <h1 className="font-bold text-bc-yellow-100">질병 검사표</h1>
+            <p className="text-xl font-bold">{localeDateString}</p>
+          </div>
+          <Button size="sm">
+            <p className="text-base font-bold">결과보기</p>
+          </Button>
+        </div>
+      </Card>
+    </Link>
   );
 };
 export default DiagnosisListItem;
