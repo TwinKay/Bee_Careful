6a7f0023c51b1e9ed6c12cd2c94dd4bb1a7f4c22
dkandjsl <dkandjsl@gmail.com>
2025-05-11 19:17:26 +0900

    feat: 벌통 상세 화면

--- FE/beecareful/src/pages/beehive/BeehiveDetailPage.tsx
+++ FE/beecareful/src/pages/beehive/BeehiveDetailPage.tsx
@@ -1,19 +1,53 @@
 import Button from '@/components/common/Button';
-import { ROUTES } from '@/config/routes';
-import { Link } from 'react-router-dom';
+import Card from '@/components/common/Card';
+import Toggle from '@/components/common/Toggle';
+import { useMemo, useState } from 'react';
+import { TMP_DIAGNOSIS_DATA } from '@/config/constants';
+import Chart from '@/components/diagnosis/Chart';
+import CardTitle from '@/components/common/CardTitle';
+import DiagnosisList from '@/components/diagnosis/DiagnosisList';
 
 const BeehiveDetailPage = () => {
+  const [isToggleLeft, setIsToggleLeft] = useState(true);
+  const recentData = useMemo(
+    () =>
+      TMP_DIAGNOSIS_DATA.filter(
+        ({ date }) =>
+          new Date(date).getTime() >
+          new Date().getTime() - (isToggleLeft ? 6 : 12) * 30 * 24 * 60 * 60 * 1000,
+      ),
+    [isToggleLeft],
+  );
+
   return (
-    <div>
-      <h1>Beehive Detail</h1>
-      <Button>
-        <Link to={ROUTES.DIAGNOSIS_DETAIL}>Go to Diagnosis Detail</Link>
-      </Button>
-      <Button>
-        {' '}
-        <Link to={ROUTES.BEEHIVES}>Go to Beehive List</Link>
-      </Button>
-    </div>
+    <>
+      <div className="flex w-full items-center justify-between">
+        <div className="flex flex-col items-start p-6">
+          <p className="text-lg font-bold">벌통이름10자미만</p>
+          <p className="font-semibold text-bc-yellow-100">벌통</p>
+        </div>
+        <Button disabled>
+          <p className="font-bold text-gray-600">장치 미연동</p>
+        </Button>
+      </div>
+      <Card className="px-0">
+        <CardTitle title="질병 감염 통계" className="px-6" />
+        <Toggle
+          onToggle={(status) => {
+            console.log(status);
+            setIsToggleLeft(status);
+          }}
+          isLeft={isToggleLeft}
+          leftLabel="6개월"
+          rightLabel="1년"
+        />
+        <Chart data={recentData} />
+      </Card>
+      <Card>
+        <CardTitle title="질병 검사 결과" />
+        <DiagnosisList data={recentData} />
+      </Card>
+    </>
   );
 };
 export default BeehiveDetailPage;
