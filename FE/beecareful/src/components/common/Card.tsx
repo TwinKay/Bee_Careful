23a259330ca5b0235be2417c5faddaed65ffd4ea
leejunghyeok <dkandjsl@gmail.com>
2025-05-12 13:25:38 +0900

    hotfix: Card 높이 fit

--- FE/beecareful/src/components/common/Card.tsx
+++ FE/beecareful/src/components/common/Card.tsx
@@ -9,7 +9,7 @@ const Card: React.FC<CardPropsType> = ({ children, className }) => {
   return (
     <div
       className={twMerge(
-        'flex h-full w-full flex-col items-center justify-start gap-6 rounded-3xl bg-white p-6',
+        'flex w-full flex-col items-center justify-start gap-6 rounded-3xl bg-white p-6',
         className,
       )}
     >
