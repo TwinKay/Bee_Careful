9d054162ba1abeca96d2af2c77349b1890267d59
박준서 <wnstj1128@naver.com>
2025-05-13 17:38:42 +0900

    feat: 벌통 상세 조회 기능 구현

--- BE/beecareful/src/main/java/com/worldbeesion/beecareful/beehive/model/dto/BeehiveDetailResponseDto.java
+++ BE/beecareful/src/main/java/com/worldbeesion/beecareful/beehive/model/dto/BeehiveDetailResponseDto.java
@@ -7,7 +7,6 @@ import java.util.List;
 public record BeehiveDetailResponseDto(
         @JsonProperty("diagnoses")
         List<BeehiveDiagnosisInfoDto> beehiveDiagnosisInfoDto,
-        PageInfoDto pageInfoDto,
         String nickname,
         Long turretId
 ) {
