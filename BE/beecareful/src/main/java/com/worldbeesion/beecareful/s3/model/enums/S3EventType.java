d5363bcdfe795483341994002f5555bc449bc4b5
Gwon Hong <gwonhong.dev@gmail.com>
2025-05-16 17:24:33 +0900

    fix : remove s3 prefix

--- BE/beecareful/src/main/java/com/worldbeesion/beecareful/s3/model/enums/S3EventType.java
+++ BE/beecareful/src/main/java/com/worldbeesion/beecareful/s3/model/enums/S3EventType.java
@@ -10,17 +10,17 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum S3EventType {
-    OBJECT_CREATED_PUT("S3:ObjectCreated:Put"),
-    OBJECT_CREATED_POST("S3:ObjectCreated:Post"),
-    OBJECT_CREATED_COPY("S3:ObjectCreated:Copy"),
-    OBJECT_CREATED_COMPLETE_MULTIPART_UPLOAD("S3:ObjectCreated:CompleteMultipartUpload"),
-    OBJECT_REMOVED_DELETE("S3:ObjectRemoved:Delete"),
-    OBJECT_REMOVED_DELETE_MARKER_CREATED("S3:ObjectRemoved:DeleteMarkerCreated"),
-    REDUCED_REDUNDANCY_LOST_OBJECT("S3:ReducedRedundancyLostObject"),
-    REPLICATION_OPERATION_FAILED("S3:Replication:OperationFailedReplication"),
-    REPLICATION_OPERATION_MISSED_THRESHOLD("S3:Replication:OperationMissedThreshold"),
-    REPLICATION_OPERATION_REPLICATED("S3:Replication:OperationReplicated"),
-    REPLICATION_OPERATION_NOT_TRACKED("S3:Replication:OperationNotTracked");
+    OBJECT_CREATED_PUT("ObjectCreated:Put"),
+    OBJECT_CREATED_POST("ObjectCreated:Post"),
+    OBJECT_CREATED_COPY("ObjectCreated:Copy"),
+    OBJECT_CREATED_COMPLETE_MULTIPART_UPLOAD("ObjectCreated:CompleteMultipartUpload"),
+    OBJECT_REMOVED_DELETE("ObjectRemoved:Delete"),
+    OBJECT_REMOVED_DELETE_MARKER_CREATED("ObjectRemoved:DeleteMarkerCreated"),
+    REDUCED_REDUNDANCY_LOST_OBJECT("ReducedRedundancyLostObject"),
+    REPLICATION_OPERATION_FAILED("Replication:OperationFailedReplication"),
+    REPLICATION_OPERATION_MISSED_THRESHOLD("Replication:OperationMissedThreshold"),
+    REPLICATION_OPERATION_REPLICATED("Replication:OperationReplicated"),
+    REPLICATION_OPERATION_NOT_TRACKED("Replication:OperationNotTracked");
 
     private final String eventName;
 }
