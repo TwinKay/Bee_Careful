3024ba5d0283c35f1654964b29cb2ffe41183271
leejunghyeok <dkandjsl@gmail.com>
2025-05-19 13:52:18 +0900

    hotfix: 진단 상세 필드명 수정

--- FE/beecareful/src/pages/diagnosis/DiagnosisDetailPage.tsx
+++ FE/beecareful/src/pages/diagnosis/DiagnosisDetailPage.tsx
@@ -15,53 +15,53 @@ const DiagnosisDetailPage: React.FC<DiagnosisDataType> = (data) => {
       imagoDisease: [
         {
           name: '진드기(응애)',
-          value: data.diagnosis.imago?.varroaCount,
-          ratio: data.diagnosis.imago?.varroaRatio,
+          value: data.result.imago?.varroaCount,
+          ratio: data.result.imago?.varroaRatio,
           color: '#E57373',
         },
         {
           name: '날개바이러스',
-          value: data.diagnosis.imago?.dwvCount,
-          ratio: data.diagnosis.imago?.dwvRatio,
+          value: data.result.imago?.dwvCount,
+          ratio: data.result.imago?.dwvRatio,
           color: '#FFB74D',
         },
         {
           name: '정상',
           value:
             data.imagoCount -
-            (data.diagnosis.imago?.varroaCount || 0) -
-            (data.diagnosis.imago?.dwvCount || 0),
-          ratio: 100 - (data.diagnosis.imago?.varroaRatio || 0),
+            (data.result.imago?.varroaCount || 0) -
+            (data.result.imago?.dwvCount || 0),
+          ratio: 100 - (data.result.imago?.varroaRatio || 0),
           color: '#E6E6E6',
         },
       ],
       larvaDisease: [
         {
           name: '진드기(응애)',
-          value: data.diagnosis.larva?.varroaCount,
-          ratio: data.diagnosis.larva?.varroaRatio,
+          value: data.result.larva?.varroaCount,
+          ratio: data.result.larva?.varroaRatio,
           color: '#E57373',
         },
         {
           name: '부저병',
-          value: data.diagnosis.larva?.foulBroodCount,
-          ratio: data.diagnosis.larva?.foulBroodRatio,
+          value: data.result.larva?.foulBroodCount,
+          ratio: data.result.larva?.foulBroodRatio,
           color: '#64B5F6',
         },
         {
           name: '석고병',
-          value: data.diagnosis.larva?.chalkBroodCount,
-          ratio: data.diagnosis.larva?.chalkBroodRatio,
+          value: data.result.larva?.chalkBroodCount,
+          ratio: data.result.larva?.chalkBroodRatio,
           color: '#81C784',
         },
         {
           name: '정상',
           value:
             data.larvaCount -
-            (data.diagnosis.larva?.varroaCount || 0) -
-            (data.diagnosis.larva?.foulBroodCount || 0) -
-            (data.diagnosis.larva?.chalkBroodCount || 0),
-          ratio: 100 - (data.diagnosis.larva?.varroaRatio || 0),
+            (data.result.larva?.varroaCount || 0) -
+            (data.result.larva?.foulBroodCount || 0) -
+            (data.result.larva?.chalkBroodCount || 0),
+          ratio: 100 - (data.result.larva?.varroaRatio || 0),
           color: '#E6E6E6',
         },
       ],
