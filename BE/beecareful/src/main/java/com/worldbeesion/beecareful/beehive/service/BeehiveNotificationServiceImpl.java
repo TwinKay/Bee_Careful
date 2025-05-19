ad618f5a668a12e5f5bcb6d9ecbb44c407e390a4
박준서 <wnstj1128@naver.com>
2025-05-19 14:37:04 +0900

    feat : 말벌 출현 시 알람 기능 구현

---
+++ BE/beecareful/src/main/java/com/worldbeesion/beecareful/beehive/service/BeehiveNotificationServiceImpl.java
@@ -0,0 +1,35 @@
+package com.worldbeesion.beecareful.beehive.service;
+
+import com.worldbeesion.beecareful.beehive.exception.BeehiveNotFoundException;
+import com.worldbeesion.beecareful.beehive.exception.TurretNotFoundException;
+import com.worldbeesion.beecareful.beehive.model.dto.BeehiveNotificationDto;
+import com.worldbeesion.beecareful.beehive.model.entity.Beehive;
+import com.worldbeesion.beecareful.beehive.model.entity.Turret;
+import com.worldbeesion.beecareful.beehive.repository.TurretRepository;
+import com.worldbeesion.beecareful.common.auth.principal.UserDetailsImpl;
+import com.worldbeesion.beecareful.notification.constant.NotificationType;
+import com.worldbeesion.beecareful.notification.model.dto.NotificationRequestDto;
+import com.worldbeesion.beecareful.notification.service.FCMService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BeehiveNotificationServiceImpl implements BeehiveNotificationService {
+
+    private static final String HORNET_APPEAR_MESSAGE = "말벌이 출현했습니다.";
+
+    private final TurretRepository turretRepository;
+    private final FCMService fcmService;
+
+
+    @Override
+    public void sendBeehiveNotification(BeehiveNotificationDto beehiveNotificationDto, UserDetailsImpl userDetails) {
+        Turret turret = turretRepository.findBySerial(beehiveNotificationDto.serial()).orElseThrow(TurretNotFoundException::new);
+        Beehive beehive = turretRepository.findBeehiveByTurretId(turret.getId()).orElseThrow(BeehiveNotFoundException::new);
+
+        NotificationRequestDto notificationRequestDto = new NotificationRequestDto(beehive.getId(), HORNET_APPEAR_MESSAGE, NotificationType.WARNING);
+
+        fcmService.alertNotificationByFCM(userDetails, notificationRequestDto);
+    }
+}
