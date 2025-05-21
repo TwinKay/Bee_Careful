b1554d1f8ca270269ad77c3940d91e7c86a87c7f
Gwon Hong <gwonhong.dev@gmail.com>
2025-05-21 14:19:37 +0900

    fix : update isolation level to read committed

--- BE/beecareful/src/main/java/com/worldbeesion/beecareful/beehive/service/DiagnosisServiceImpl.java
+++ BE/beecareful/src/main/java/com/worldbeesion/beecareful/beehive/service/DiagnosisServiceImpl.java
@@ -24,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -112,7 +113,7 @@ public class DiagnosisServiceImpl implements DiagnosisService {
     }
 
     @Override
-    @Transactional
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public void runDiagnosis(Long diagnosisId) {
         log.info("Starting diagnosis process for diagnosisId: {}", diagnosisId);
 
